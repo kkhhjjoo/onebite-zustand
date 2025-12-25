9b39bd3f8e1e7f44a5346b11dd2e2f06c915e85f
kkhhjjoo <khjoo1203@gmail.com>
2025-12-25 14:39:59 +0900

    zustand 미들웨어:immer

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { combine } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
 // type Store = {
 //   count: number;
@@ -10,22 +11,22 @@ import { combine } from "zustand/middleware";
 // };
 
 export const useCountStore = create(
-  combine({ count: 0 }, (set, get) => ({
-    actions: {
-      increaseOne: () => {
-        // const count = get().count;
-        // set({ count: count + 1 });
-        set((state) => ({
-          count: state.count + 1,
-        }));
+  immer(
+    combine({ count: 0 }, (set, get) => ({
+      actions: {
+        increaseOne: () => {
+          set((state) => {
+            state.count += 1;
+          });
+        },
+        decreaseOne: () => {
+          set((state) => {
+            state.count -= 1;
+          });
+        },
       },
-      decreaseOne: () => {
-        set((state) => ({
-          count: state.count - 1,
-        }));
-      },
-    },
-  })),
+    })),
+  ),
 );
 
 // export const useCountStore = create<Store>((set, get) => {
